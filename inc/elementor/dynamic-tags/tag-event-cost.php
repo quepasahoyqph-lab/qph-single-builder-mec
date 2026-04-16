8f0977d17f58a0ce6463667e4282fc7cae5cbf42
quepasahoyqph-lab <quepasahoyqph@gmail.com>
2026-04-15 21:55:35 -0500

    Update and rename tag-event-cost to tag-event-cost.php

---
+++ inc/elementor/dynamic-tags/tag-event-cost.php
@@ -0,0 +1,27 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+class QPH_Tag_Event_Cost extends \Elementor\Core\DynamicTags\Tag {
+
+    use QPH_ESB_Widget_Helper;
+
+    public function get_name() {
+        return 'qph-event-cost';
+    }
+
+    public function get_title() {
+        return 'Event Cost';
+    }
+
+    public function get_group() {
+        return 'qph-mec';
+    }
+
+    public function get_categories() {
+        return [\Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY];
+    }
+
+    public function render() {
+        echo esc_html($this->get_cost());
+    }
+}
