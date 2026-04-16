713ed838c80ced78884ef7dbba1bcd6f40021096
quepasahoyqph-lab <quepasahoyqph@gmail.com>
2026-04-15 22:01:45 -0500

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,28 @@
+(function($) {
+    'use strict';
 
+    // Toggle opciones del builder en MEC Settings
+    function qphToggleBuilderOptions() {
+        var val = $('input[name="mec[settings][single_single_style]"]:checked').val();
+        var $wraps = $(
+            '#mec_settings_single_event_single_default_builder_wrap,'
+            + '#mec_settings_single_event_single_modal_default_builder_wrap,'
+            + '#mec_settings_custom_event_for_set_settings_wrap'
+        );
+        if (val === 'builder') {
+            $wraps.slideDown(300);
+        } else {
+            $wraps.slideUp(300);
+        }
+    }
+
+    $(document).ready(function() {
+        $(document).on(
+            'change',
+            'input[name="mec[settings][single_single_style]"]',
+            qphToggleBuilderOptions
+        );
+        qphToggleBuilderOptions();
+    });
+
+})(jQuery);
